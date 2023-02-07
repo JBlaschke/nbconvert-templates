b27fff17a55e5260d310c7e53ecf128468cdae82
Johannes Blaschke <johannes@blaschke.science>
2023-02-07 10:05:01 -0800

    fix templates and installer

--- setup/installer.lua
+++ setup/installer.lua
@@ -1,8 +1,9 @@
 #!/usr/bin/env lua
 
-local gears = require "opt.lib.gears"
-local sh    = require "sh"
-local F     = require "F"
+local gears    = require "opt.lib.gears"
+local lustache = require "lustache"
+local sh       = require "sh"
+local F        = require "F"
 
 sh.__raise_errors = false
 
@@ -37,12 +38,29 @@ end
 
 if has_nbconvert_config_file then
     print(F"{jupyter_config_dir}/jupyter_nbconvert_config.py already exists")
-    os.exit(1)
+    print(F"  => NOT creating symlink to {jupyter_config_dir}")
+else
+    sh.ln(
+        "-s",
+        F"{app_dir}/setup/var/jupyter_nbconvert_config.py",
+        jupyter_config_dir
+    )
 end
 
+print(F"Generating config in {app_dir}/setup/var")
 
-sh.ln(
-    "-s",
-    F"{app_dir}/setup/share/jupyter_nbconvert_config.py",
-    jupyter_config_dir
-)
+-- read template file
+local file = io.open(F"{app_dir}/setup/share/jupyter_nbconvert_config.py", "r")
+local tmpl_file = file:read("*all")
+file:close()
+
+-- fill in script template
+local tmpl_params = {
+    MY_TEMPLATES = F"{app_dir}/templates"
+}
+local script = lustache:render(tmpl_file, tmpl_params)
+
+-- save script file
+local file = io.open(F"{app_dir}/setup/var/jupyter_nbconvert_config.py", "w")
+file:write(script)
+file:close()
--- setup/share/jupyter_nbconvert_config.py
+++ setup/share/jupyter_nbconvert_config.py
@@ -6,5 +6,6 @@ from pathlib import Path
 # Location of this file: nbconvert-templates/setup/share
 # Location of templates: nbconvert-templates/templates
 # => Relative path: ../../templates
-my_templates = Path(__file__).resolve().parent.parent.joinpath("templates")
-c.TemplateExporter.extra_template_basedirs = [my_templates]
+# my_templates = Path(__file__).resolve().parent.parent.joinpath("templates").absolute()
+# my_templates = "/home/blaschke/Developer/nbconvert-templates/templates/"
+c.TemplateExporter.extra_template_basedirs = ["{{{MY_TEMPLATES}}}"]
