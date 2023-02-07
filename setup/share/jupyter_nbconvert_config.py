aeaedf1e68b61b401dae4a5390d1fa27e8c62207
Johannes Blaschke <johannes@blaschke.science>
2023-02-07 10:06:22 -0800

    cleanup

--- setup/share/jupyter_nbconvert_config.py
+++ setup/share/jupyter_nbconvert_config.py
@@ -3,9 +3,4 @@
 c = get_config()  #noqa
 
 from pathlib import Path
-# Location of this file: nbconvert-templates/setup/share
-# Location of templates: nbconvert-templates/templates
-# => Relative path: ../../templates
-# my_templates = Path(__file__).resolve().parent.parent.joinpath("templates").absolute()
-# my_templates = "/home/blaschke/Developer/nbconvert-templates/templates/"
 c.TemplateExporter.extra_template_basedirs = ["{{{MY_TEMPLATES}}}"]
